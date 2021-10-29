bc38405a64c3cefd72a4e69bac2e6f5bed41f340
Nick Sullivan <nicholassull@gmail.com>
2021-10-29 16:15:12 -0700

    add JS toggles to remaining terms

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,58 @@ $(document).ready(function() {
   $("#term1").click(function () {
     $("#def1").toggle();
   });
+  $("#term2").click(function () {
+    $("#def2").toggle();
+  });
+  $("#term3").click(function () {
+    $("#def3").toggle();
+  });
+  $("#term4").click(function () {
+    $("#def4").toggle();
+  });
+  $("#term5").click(function () {
+    $("#def5").toggle();
+  });
+  $("#term6").click(function () {
+    $("#def6").toggle();
+  });
+  $("#term7").click(function () {
+    $("#def7").toggle();
+  });
+  $("#term8").click(function () {
+    $("#def8").toggle();
+  });
+  $("#term9").click(function () {
+    $("#def9").toggle();
+  });
+  $("#term10").click(function () {
+    $("#def10").toggle();
+  });
+  $("#term11").click(function () {
+    $("#def11").toggle();
+  });
+  $("#term12").click(function () {
+    $("#def12").toggle();
+  });
+  $("#term13").click(function () {
+    $("#def13").toggle();
+  });
+  $("#term14").click(function () {
+    $("#def14").toggle();
+  });
+  $("#term15").click(function () {
+    $("#def15").toggle();
+  });
+  $("#term16").click(function () {
+    $("#def16").toggle();
+  });
+  $("#term17").click(function () {
+    $("#def17").toggle();
+  });
+  $("#term8").click(function () {
+    $("#def18").toggle();
+  });
+  $("#term19").click(function () {
+    $("#def19").toggle();
+  });
 });
